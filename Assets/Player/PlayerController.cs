7dafc5b9e5937bb099b7c3031b7f80df0f0f457c
Arturproshak <123242869+Arturproshak@users.noreply.github.com>
2024-11-02 15:33:54 +0300

    move

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A и D для движения влево и вправо
+        float moveZ = Input.GetAxis("Vertical");   // W и S для движения вперед и назад
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, mouseX, 0); // Поворот персонажа по оси Y
+    }
+}
