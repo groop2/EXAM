60098e6d8bcf0038e21987c59f6049e2d4a5b8dd
Arturproshak <123242869+Arturproshak@users.noreply.github.com>
2024-11-02 18:52:31 +0300

    и

---
+++ Assets/Scripts/Button1Trigger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button1Trigger : MonoBehaviour
+{
+    public GameObject dialogCanvas;
+    public Text questionText;
+    public Text responseText;
+    public GameObject door;
+    public DoorController doorController;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            dialogCanvas.SetActive(true);
+            questionText.text = "Кого вы спасете: ребенка или маму?";
+            responseText.text = "Вы выбрали ребенка.";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            dialogCanvas.SetActive(false);
+            questionText.text = "";
+            responseText.text = "";
+        }
+    }
+
+    public void Choose()
+    {
+        responseText.text += "\nСделал ли ты правильный выбор?";
+        Invoke("OpenDoor", 3f);
+    }
+
+    private void OpenDoor()
+    {
+        dialogCanvas.SetActive(false);
+        responseText.text = "";
+        doorController.IsOpen = true;
+    }
+}
